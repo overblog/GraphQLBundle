453a3033e7c8ca273862462a39d5e4a90074697d
Florent Cailhol <florent.cailhol@gmail.com>
2016-02-05 14:44:36 +0100

    Add RawId helper

--- Relay/Node/RawIdField.php
+++ Relay/Node/RawIdField.php
@@ -8,29 +8,24 @@ use GraphQL\Type\Definition\Type;
 use GraphQL\Utils;
 use Overblog\GraphBundle\Definition\FieldInterface;
 
-class GlobalIdField implements FieldInterface
+class RawIdField implements FieldInterface
 {
     public function toFieldsDefinition(array $config)
     {
         Config::validate($config, [
             'name' => Config::STRING | Config::REQUIRED,
-            'typeName' => Config::STRING,
             'idFetcher' => Config::CALLBACK
         ]);
 
         $name = $config['name'];
-        $typeName = isset($config['typeName']) ? $config['typeName'] : null;
         $idFetcher = isset($config['idFetcher']) ? $config['idFetcher'] : null;
 
         return [
             'name' => $name,
-            'description' => 'The ID of an object',
-            'type' => Type::nonNull(Type::id()),
-            'resolve' => function($obj, $args, ResolveInfo $info) use ($idFetcher, $typeName) {
-                return GlobalId::toGlobalId(
-                    !empty($typeName) ? $typeName : $info->parentType->name,
-                    is_callable($idFetcher) ? $idFetcher($obj, $info) : $obj->id
-                );
+            'description' => 'The raw ID of an object',
+            'type' => Type::nonNull(Type::int()),
+            'resolve' => function($obj, $args, ResolveInfo $info) use ($idFetcher) {
+                return is_callable($idFetcher) ? $idFetcher($obj, $info) : $obj->id;
             }
         ];
     }
