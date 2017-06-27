7daa4c5eff07d7410f7a2f785177d81290cab05b
Andreas Heiberg <andreas@heiberg.io>
2017-06-27 13:26:25 +0100

    lazy load resolvers in overblog to avoid having special cases in TaggedServiceMappingPass
    I see now that in fact there are many things that relu on this…
    
    - TypeResolver
    - ResolverResolver
    - MutationResolver
    - AccessResolver

--- Resolver/ResolverResolver.php
+++ Resolver/ResolverResolver.php
@@ -11,8 +11,39 @@
 
 namespace Overblog\GraphQLBundle\Resolver;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 class ResolverResolver extends AbstractProxyResolver
 {
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * ResolverResolver constructor.
+     *
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function getSolution($name)
+    {
+        $solution = parent::getSolution($name);
+
+        if (! $solution)  {
+            $typeOptions = $this->getSolutionOptions($name);
+            if ($typeOptions and $this->container->has($typeOptions['id'])) {
+                $solution = $this->container->get($typeOptions['id']);
+            }
+        }
+
+        return $solution;
+    }
+
     protected function unresolvableMessage($alias)
     {
         return sprintf('Unknown resolver with alias "%s" (verified service tag)', $alias);
