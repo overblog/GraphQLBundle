bee1eb3565fa81d4ee5aa070a0a08e4352f8e9e2
Andreas Heiberg <andreas@heiberg.io>
2017-06-27 12:18:58 +0100

    Initial experimentation in LazyTypeResolution strategy

--- Definition/Builder/SchemaBuilder.php
+++ Definition/Builder/SchemaBuilder.php
@@ -13,6 +13,7 @@ namespace Overblog\GraphQLBundle\Definition\Builder;
 
 use GraphQL\Schema;
 use GraphQL\Type\Definition\Config;
+use GraphQL\Type\LazyResolution;
 use Overblog\GraphQLBundle\Resolver\ResolverInterface;
 
 class SchemaBuilder
@@ -46,11 +47,42 @@ class SchemaBuilder
         $mutation = $this->typeResolver->resolve($mutationAlias);
         $subscription = $this->typeResolver->resolve($subscriptionAlias);
 
-        return new Schema([
+        $config = [
             'query' => $query,
             'mutation' => $mutation,
             'subscription' => $subscription,
-            'types' => $this->typeResolver->getSolutions(),
-        ]);
+            'types' => $this->typeResolver->getSolutions()
+        ];
+
+        $descriptorFile = __DIR__.'/../../../../../var/cache/dev/overblog/graph-bundle/schema-descriptor.php';
+        if (file_exists($descriptorFile)) {
+            $descriptor = include $descriptorFile;
+
+            $config['typeResolution'] = new LazyResolution($descriptor, function($typeName) {
+                $classNames = [
+                    "Overblog\\GraphQLBundle\\__DEFINITIONS__\\{$typeName}Type",
+                    "GraphQL\\Type\\Definition\\{$typeName}Type"
+                ];
+
+                foreach ($classNames as $className) {
+                    if (class_exists($className)) {
+                        return new $className;
+                    }
+                }
+
+                return null;
+            });
+        }
+
+        $schema = new Schema($config);
+
+        if (! file_exists($descriptorFile)) {
+            file_put_contents(
+                $descriptorFile,
+                "<?php\n return " . var_export($schema->getDescriptor(), true) . ';'
+            );
+        }
+
+        return $schema;
     }
 }
