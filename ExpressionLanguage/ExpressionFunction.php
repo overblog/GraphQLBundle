5ed5e4c4c3481f89ddee9836f7ff0aedc0d8b610
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2017-10-13 16:04:18 +0200

    Dropping license header from all files

--- ExpressionLanguage/ExpressionFunction.php
+++ ExpressionLanguage/ExpressionFunction.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the OverblogGraphQLBundle package.
- *
- * (c) Overblog <http://github.com/overblog/>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Overblog\GraphQLBundle\ExpressionLanguage;
 
 use Symfony\Component\ExpressionLanguage\ExpressionFunction as BaseExpressionFunction;
