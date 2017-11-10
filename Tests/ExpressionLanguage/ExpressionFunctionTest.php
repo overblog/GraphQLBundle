1d76959268f35ed7cb10c3be74bbc45033a036d0
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2017-11-10 22:55:54 +0100

    Tests: Removed ExpressionTest::fromPhp since it isn't used

--- Tests/ExpressionLanguage/ExpressionFunctionTest.php
+++ Tests/ExpressionLanguage/ExpressionFunctionTest.php
@@ -7,14 +7,6 @@ use PHPUnit\Framework\TestCase;
 
 class ExpressionFunctionTest extends TestCase
 {
-    public function testFunctionDoesNotExist()
-    {
-        $this->expectException(\InvalidArgumentException::class);
-        $this->expectExceptionMessage('PHP function "fn_does_not_exist" does not exist.');
-
-        ExpressionFunction::fromPhp('fn_does_not_exist');
-    }
-
     public function testThereIsNoEvaluator()
     {
         $this->expectException(\RuntimeException::class);
