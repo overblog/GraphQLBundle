783c48dba53a5fc1926e5878ef7a86b81f65470c
Maxime AURIAU <maxime.auriau@gmail.com>
2018-10-01 17:38:13 +0200

    Log errors from graphQl result

--- src/EventListener/ErrorHandlerListener.php
+++ src/EventListener/ErrorHandlerListener.php
@@ -6,9 +6,13 @@ namespace Overblog\GraphQLBundle\EventListener;
 
 use Overblog\GraphQLBundle\Error\ErrorHandler;
 use Overblog\GraphQLBundle\Event\ExecutorResultEvent;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 
 final class ErrorHandlerListener
 {
+    public const DEFAULT_LOGGER_SERVICE = 'logger';
+
     /** @var ErrorHandler */
     private $errorHandler;
 
@@ -18,16 +22,29 @@ final class ErrorHandlerListener
     /** @var bool */
     private $debug;
 
-    public function __construct(ErrorHandler $errorHandler, $throwException = false, $debug = false)
-    {
+    /** @var LoggerInterface */
+    private $logger;
+
+    public function __construct(
+        ErrorHandler $errorHandler,
+        LoggerInterface $logger,
+        bool $throwException = false,
+        bool $debug = false
+    ) {
         $this->errorHandler = $errorHandler;
         $this->throwException = $throwException;
         $this->debug = $debug;
+        $this->logger = null === $logger ? new NullLogger() : $logger;
     }
 
     public function onPostExecutor(ExecutorResultEvent $executorResultEvent): void
     {
         $result = $executorResultEvent->getResult();
         $this->errorHandler->handleErrors($result, $this->throwException, $this->debug);
+        $result = $result->toArray();
+
+        if (isset($result['errors'])) {
+            $this->logger->error(__METHOD__.' : '.\json_encode($result['errors']));
+        }
     }
 }
