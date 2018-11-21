b9d04eca9798a1fe2791074bc0c1c18e6f4d17ff
Aurélien David <spyl94@gmail.com>
2018-11-21 19:17:11 +0100

    Fix using custom names on a relay-mutation Input and Payload

--- src/Relay/Mutation/InputDefinition.php
+++ src/Relay/Mutation/InputDefinition.php
@@ -8,13 +8,14 @@ final class InputDefinition implements MappingInterface
 {
     public function toMappingDefinition(array $config)
     {
+        $alias = \preg_replace('/(.*)?Type$/', '$1', $config['class_name']);
         $name = $config['name'];
         $name = \preg_replace('/(.*)?Input$/', '$1', $name).'Input';
 
         $inputFields = empty($config['fields']) || !\is_array($config['fields']) ? [] : $config['fields'];
 
         return [
-            $name => [
+            $alias => [
                 'type' => 'input-object',
                 'config' => [
                     'name' => $name,
--- src/Relay/Mutation/PayloadDefinition.php
+++ src/Relay/Mutation/PayloadDefinition.php
@@ -8,12 +8,13 @@ final class PayloadDefinition implements MappingInterface
 {
     public function toMappingDefinition(array $config)
     {
+        $alias = \preg_replace('/(.*)?Type$/', '$1', $config['class_name']);
         $name = $config['name'];
         $name = \preg_replace('/(.*)?Payload$/', '$1', $name).'Payload';
         $outputFields = empty($config['fields']) || !\is_array($config['fields']) ? [] : $config['fields'];
 
         return [
-            $name => [
+            $alias => [
                 'type' => 'object',
                 'config' => [
                     'name' => $name,
