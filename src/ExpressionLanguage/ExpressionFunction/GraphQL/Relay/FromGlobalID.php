4e016726b3587356e4e10a12a1bfc2fa1a441b75
murtukov <murtukov@gmail.com>
2019-09-12 14:30:45 +0200

    Fix GlobalId namespace alias

--- src/ExpressionLanguage/ExpressionFunction/GraphQL/Relay/FromGlobalID.php
+++ src/ExpressionLanguage/ExpressionFunction/GraphQL/Relay/FromGlobalID.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Overblog\GraphQLBundle\ExpressionLanguage\ExpressionFunction\GraphQL\Relay;
 
 use Overblog\GraphQLBundle\ExpressionLanguage\ExpressionFunction;
-use Overblog\GraphQLBundle\Relay\Node\GlobalId;
+use Overblog\GraphQLBundle\Relay\Node\GlobalId as GlobalIdHelper;
 
 final class FromGlobalID extends ExpressionFunction
 {
@@ -14,10 +14,10 @@ final class FromGlobalID extends ExpressionFunction
         parent::__construct(
             'fromGlobalId',
             function (string $globalId): string {
-                return \sprintf('\%s::fromGlobalId(%s)', GlobalId::class, $globalId);
+                return \sprintf('\%s::fromGlobalId(%s)', GlobalIdHelper::class, $globalId);
             },
-            function ($arguments, $globalId): array {
-                return GlobalId::fromGlobalId($globalId);
+            function ($_, $globalId): array {
+                return GlobalIdHelper::fromGlobalId($globalId);
             }
         );
     }
