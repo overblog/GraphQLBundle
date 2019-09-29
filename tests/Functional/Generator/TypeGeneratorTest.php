fed7dba6f07f20889c3760c20d26684946b7f828
Timur Murtukov <murtukov@gmail.com>
2019-09-29 17:36:50 +0200

    Update tests
    - Move the test for non-existent constraint
    - Create test for the option `cascade` on scalar types
    - Update the ExpectedErrors class according to the new error shape
    -

--- tests/Functional/Generator/TypeGeneratorTest.php
+++ tests/Functional/Generator/TypeGeneratorTest.php
@@ -53,4 +53,26 @@ class TypeGeneratorTest extends TestCase
             )['errors'][0]['message']
         );
     }
+
+    /**
+     * Defining the `cascade` validation option on
+     * scalar types should throw an exception.
+     */
+    public function testCascadeOnScalarasThrowsException(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage("Cascade validation cannot be applied to built-in types.");
+
+        parent::setUp();
+        static::bootKernel(['test_case' => 'cascadeOnScalars']);
+    }
+
+    public function testNonExistentConstraintThrowsException(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage("Constraint class 'Symfony\Component\Validator\Constraints\BlahBlah' doesn't exist.");
+
+        parent::setUp();
+        static::bootKernel(['test_case' => 'nonexistentConstraint']);
+    }
 }
--- tests/Functional/Validator/ExpectedErrors.php
+++ tests/Functional/Validator/ExpectedErrors.php
@@ -7,80 +7,140 @@ namespace Overblog\GraphQLBundle\Tests\Functional\Validator;
 class ExpectedErrors
 {
     public const SIMPLE_VALIDATION = [
-        'message' => 'Invalid data set',
-        'extensions' => ['category' => 'ArgumentsValidationException'],
-        'path' => ['simpleValidation'],
-        'state' => [
-            'username' => ['This value is too short. It should have 5 characters or more.'],
+        'message' => 'validation',
+        'extensions' => [
+            'category' => 'arguments_validation_error',
+            'validation' => [
+                'username' => [
+                    [
+                        'message' => 'This value is too short. It should have 5 characters or more.',
+                        'code' => '9ff3fdc4-b214-49db-8718-39c315e33d45',
+                    ]
+                ]
+            ]
         ],
-        'code' => [
-            'username' => ['9ff3fdc4-b214-49db-8718-39c315e33d45'],
+        'locations' => [
+            [
+                'line' => 3,
+                'column' => 17,
+            ]
         ],
+        'path' => ['simpleValidation']
     ];
 
     public const LINKED_CONSTRAINTS = [
-        'message' => 'Invalid data set',
-        'extensions' => ['category' => 'ArgumentsValidationException'],
-        'path' => ['linkedConstraintsValidation'],
-        'state' => [
-            '' => ['This value is not valid.'],
-            'string1' => ['This value should be equal to "Lorem Ipsum".'],
-            'string2' => ['This value should be equal to "Dolor Sit Amet".'],
-            'string3' => [
-                'This value should be equal to "{"text":"Lorem Ipsum"}".',
-                'This value should be valid JSON.',
-            ],
+        'message' => 'validation',
+        'extensions' => [
+            'category' => 'arguments_validation_error',
+            'validation' => [
+                '' => [
+                    ['message' => 'This value is not valid.', 'code' => '6b3befbc-2f01-4ddf-be21-b57898905284']
+                ],
+                'string1' => [
+                    ['message' => 'This value should be equal to "Lorem Ipsum".', 'code' => '478618a7-95ba-473d-9101-cabd45e49115']
+                ],
+                'string2' => [
+                    ['message' => 'This value should be equal to "Dolor Sit Amet".', 'code' => '478618a7-95ba-473d-9101-cabd45e49115']
+                ],
+                'string3' => [
+                    ['message' => 'This value should be equal to "{"text":"Lorem Ipsum"}".', 'code' => '478618a7-95ba-473d-9101-cabd45e49115'],
+                    ['message' => 'This value should be valid JSON.', 'code' => '0789c8ad-2d2b-49a4-8356-e2ce63998504']
+                ]
+            ]
         ],
-        'code' => [
-            '' => ['6b3befbc-2f01-4ddf-be21-b57898905284'],
-            'string1' => ['478618a7-95ba-473d-9101-cabd45e49115'],
-            'string2' => ['478618a7-95ba-473d-9101-cabd45e49115'],
-            'string3' => [
-                '478618a7-95ba-473d-9101-cabd45e49115',
-                '0789c8ad-2d2b-49a4-8356-e2ce63998504',
-            ],
-        ],
+        'locations' => [['line' => 3, 'column' => 17]],
+        'path' => ['linkedConstraintsValidation']
     ];
 
     public const COLLECTION = [
-        'message' => 'Invalid data set',
-        'extensions' => ['category' => 'ArgumentsValidationException'],
-        'path' => ['collectionValidation'],
-        'state' => [
-            'addresses[0].street' => ['This value is too short. It should have 10 characters or more.'],
-            'addresses[0].zipCode' => ['This value is not valid.'],
-            'addresses[0].period.endDate' => ['This value should be greater than "2020-01-01".'],
-            'emails' => ['This collection should contain only unique elements.', 'This collection should contain 3 elements or more.'],
-            'emails[0]' => ['The email ""nonUniqueString"" is not a valid email.'],
-            'emails[1]' => ['The email ""nonUniqueString"" is not a valid email.'],
+        'message' => 'validation',
+        'extensions' => [
+            'category' => 'arguments_validation_error',
+            'validation' => [
+                'addresses[0].street' => [
+                    [
+                        'message' => 'This value is too short. It should have 10 characters or more.',
+                        'code' => '9ff3fdc4-b214-49db-8718-39c315e33d45'
+                    ],
+                ],
+                'addresses[0].zipCode' => [
+                    [
+                        'message' => 'This value is not valid.',
+                        'code' => '6b3befbc-2f01-4ddf-be21-b57898905284'
+                    ]
+                ],
+                'addresses[0].period.endDate' => [
+                    [
+                        'message' => 'This value should be greater than "2020-01-01".',
+                        'code' => '778b7ae0-84d3-481a-9dec-35fdb64b1d78',
+                    ],
+                ],
+                'emails' => [
+                    [
+                        'message' => 'This collection should contain only unique elements.',
+                        'code' => '7911c98d-b845-4da0-94b7-a8dac36bc55a',
+                    ],
+                    [
+                        'message' => 'This collection should contain 3 elements or more.',
+                        'code' => 'bef8e338-6ae5-4caf-b8e2-50e7b0579e69',
+                    ],
+                ],
+                'emails[0]' => [
+                    [
+                        'message' => 'The email ""nonUniqueString"" is not a valid email.',
+                        'code' => 'bd79c0ab-ddba-46cc-a703-a7a4b08de310',
+                    ],
+                ],
+                'emails[1]' =>[
+                    [
+                        'message' => 'The email ""nonUniqueString"" is not a valid email.',
+                        'code' => 'bd79c0ab-ddba-46cc-a703-a7a4b08de310',
+                    ],
+                ],
+            ],
         ],
-        'code' => [
-            'addresses[0].street' => ['9ff3fdc4-b214-49db-8718-39c315e33d45'],
-            'addresses[0].zipCode' => ['6b3befbc-2f01-4ddf-be21-b57898905284'],
-            'addresses[0].period.endDate' => ['778b7ae0-84d3-481a-9dec-35fdb64b1d78'],
-            'emails' => ['7911c98d-b845-4da0-94b7-a8dac36bc55a', 'bef8e338-6ae5-4caf-b8e2-50e7b0579e69'],
-            'emails[0]' => ['bd79c0ab-ddba-46cc-a703-a7a4b08de310'],
-            'emails[1]' => ['bd79c0ab-ddba-46cc-a703-a7a4b08de310'],
-        ],
+        'locations' => [['line' => 3, 'column' => 17]],
+        'path' => ['collectionValidation']
     ];
 
     public const CASCADE_WITH_GROUPS = [
-        'message' => 'Invalid data set',
-        'extensions' => ['category' => 'ArgumentsValidationException'],
-        'path' => ['cascadeValidationWithGroups'],
-        'state' => [
-            'address.street' => ['This value is too short. It should have 10 characters or more.'],
-            'address.zipCode' => ['This value is not valid.'],
-            'address.period.endDate' => ['This value should be greater than "2020-01-01".'],
-            'address.city' => ['The value you selected is not a valid choice.'],
-            'birthdate.day' => ['This value should be 31 or less.'],
+        'message' => 'validation',
+        'extensions' => [
+            'category' => 'arguments_validation_error',
+            'validation' => [
+                'address.street' => [
+                    [
+                        'message' => 'This value is too short. It should have 10 characters or more.',
+                        'code' => '9ff3fdc4-b214-49db-8718-39c315e33d45'
+                    ],
+                ],
+                'address.zipCode' => [
+                    [
+                        'message' => 'This value is not valid.',
+                        'code' => '6b3befbc-2f01-4ddf-be21-b57898905284'
+                    ],
+                ],
+                'address.period.endDate' => [
+                    [
+                        'message' => 'This value should be greater than "2020-01-01".',
+                        'code' => '778b7ae0-84d3-481a-9dec-35fdb64b1d78'
+                    ],
+                ],
+                'address.city' => [
+                    [
+                        'message' => 'The value you selected is not a valid choice.',
+                        'code' => '8e179f1b-97aa-4560-a02f-2a8b42e49df7'
+                    ],
+                ],
+                'birthdate.day' => [
+                    [
+                        'message' => 'This value should be 31 or less.',
+                        'code' => '2d28afcb-e32e-45fb-a815-01c431a86a69'
+                    ]
+                ]
+            ]
         ],
-        'code' => [
-            'address.street' => ['9ff3fdc4-b214-49db-8718-39c315e33d45'],
-            'address.zipCode' => ['6b3befbc-2f01-4ddf-be21-b57898905284'],
-            'address.period.endDate' => ['778b7ae0-84d3-481a-9dec-35fdb64b1d78'],
-            'address.city' => ['8e179f1b-97aa-4560-a02f-2a8b42e49df7'],
-            'birthdate.day' => ['2d28afcb-e32e-45fb-a815-01c431a86a69'],
-        ],
+        'locations' => [['line' => 3, 'column' => 17]],
+        'path' => ['cascadeValidationWithGroups']
     ];
 }
