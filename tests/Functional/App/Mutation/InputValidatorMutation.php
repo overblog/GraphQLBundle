635f86eccc17e72b5f5538b44cfb808bbe595bae
Timur Murtukov <murtukov@gmail.com>
2019-10-29 07:16:37 +0100

    Add auto validation (#593)
    * Add auto validation
    
    - Make auto validation turned on by default
    - Add the __invoke method to InputValidator for short syntax
    - Replace RuntimeException with GeneratorException in the Generator
    - Add new option `validationGroups`
    - Add new tests
    - Update documentation
    - Add new expression variable `errors`

--- tests/Functional/App/Mutation/InputValidatorMutation.php
+++ tests/Functional/App/Mutation/InputValidatorMutation.php
@@ -6,24 +6,43 @@ namespace Overblog\GraphQLBundle\Tests\Functional\App\Mutation;
 
 use Overblog\GraphQLBundle\Definition\Argument;
 use Overblog\GraphQLBundle\Definition\Resolver\MutationInterface;
+use Overblog\GraphQLBundle\Error\ResolveErrors;
 use Overblog\GraphQLBundle\Validator\Exception\ArgumentsValidationException;
 use Overblog\GraphQLBundle\Validator\InputValidator;
 
 class InputValidatorMutation implements MutationInterface
 {
     /**
-     * @param Argument       $args
-     * @param InputValidator $validator
+     * @param Argument            $args
+     * @param InputValidator|null $validator
      *
      * @return bool
      *
      * @throws ArgumentsValidationException
      */
-    public function mutationMock(Argument $args, InputValidator $validator): bool
+    public function mutationMock(Argument $args, ?InputValidator $validator = null): bool
     {
-        $validator->validate($args['groups']);
+        if (null !== $validator) {
+            $validator->validate($args['groups']);
+        }
 
         return true;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function mutationErrors(ResolveErrors $errors): bool
+    {
+        $violations = $errors->getValidationErrors();
+
+        if ($violations && 1 == $violations->count()) {
+            return true;
+        } elseif ($violations && 0 === $violations->count()) {
+            return false;
+        }
+
+        throw new \Exception("The injected variable `errors` doesn't contain an expected amount of violations.");
     }
 
     public function noValidation(): bool
