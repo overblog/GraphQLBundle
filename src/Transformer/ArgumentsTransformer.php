10942eeff219099b293719a7484f63a529e483a4
Vincent <vincent@fullstack.pro>
2020-01-21 15:26:11 +0100

    ArgumentTransformer & validator component

--- src/Transformer/ArgumentsTransformer.php
+++ src/Transformer/ArgumentsTransformer.php
@@ -152,8 +152,9 @@ class ArgumentsTransformer
         $type = \substr($argType, $isMultiple ? 1 : 0, $endIndex > 0 ? -$endIndex : \strlen($argType));
 
         $result = $this->populateObject($this->getType($type, $info), $data, $isMultiple, $info);
-        $errors = new ConstraintViolationList();
+
         if ($this->validator) {
+            $errors = new ConstraintViolationList();
             if (\is_object($result)) {
                 $errors = $this->validator->validate($result);
             }
@@ -166,13 +167,13 @@ class ArgumentsTransformer
                     }
                 }
             }
+            
+            if (\count($errors) > 0) {
+                throw new InvalidArgumentError($argName, $errors);
+            }
         }
 
-        if (\count($errors) > 0) {
-            throw new InvalidArgumentError($argName, $errors);
-        } else {
-            return $result;
-        }
+        return $result;
     }
 
     /**
