3c72e7606f929d58e2102efc7cc4284afce33835
murtukov <murtukov@gmail.com>
2020-06-02 19:04:35 +0200

    Replace embedded lib with composer dependency

--- lib/php-code-generator/src/Exception/UnrecognizedValueTypeException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Murtukov\PHPCodeGenerator\Exception;
-
-class UnrecognizedValueTypeException extends \Exception
-{
-}
--- lib/php-code-generator/src/GeneratorInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Murtukov\PHPCodeGenerator;
-
-interface GeneratorInterface
-{
-    public function __toString(): string;
-}
