1874dbb61ae3e1c723de86257ca0af27b696dbfb
murtukov <murtukov@gmail.com>
2020-06-25 01:03:43 +0200

    Update generator library to v0.1

--- src/Generator/Collection.php
+++ src/Generator/Collection.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Overblog\GraphQLBundle\Generator;
 
-use Murtukov\PHPCodeGenerator\Arrays\AssocArray as BaseArray;
+use Murtukov\PHPCodeGenerator\Collection as BaseCollection;
 use Overblog\GraphQLBundle\Generator\Converter\ExpressionConverter;
 
 /**
  * Extends the default AssocArray to properly convert expressions.
  */
-class AssocArray extends BaseArray
+class Collection extends BaseCollection
 {
     /**
      * Mark additional converters to be used by convertion of array values.
