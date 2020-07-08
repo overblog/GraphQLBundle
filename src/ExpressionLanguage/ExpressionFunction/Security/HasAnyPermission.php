f66fcc9279b92313b66f5b905a7009143230e5fa
Stefan Gehrig <gehrig@teqneers.de>
2020-07-08 10:16:11 +0200

    removes security dependency from HasAnyPermission function constructor

--- src/ExpressionLanguage/ExpressionFunction/Security/HasAnyPermission.php
+++ src/ExpressionLanguage/ExpressionFunction/Security/HasAnyPermission.php
@@ -5,19 +5,18 @@ declare(strict_types=1);
 namespace Overblog\GraphQLBundle\ExpressionLanguage\ExpressionFunction\Security;
 
 use Overblog\GraphQLBundle\ExpressionLanguage\ExpressionFunction;
-use Overblog\GraphQLBundle\Security\Security;
 
 final class HasAnyPermission extends ExpressionFunction
 {
-    public function __construct(Security $security)
+    public function __construct()
     {
         parent::__construct(
             'hasAnyPermission',
             static function ($object, $permissions): string {
                 return \sprintf('$globalVariable->get(\'security\')->hasAnyPermission(%s, %s)', $object, $permissions);
             },
-            function ($_, $object, $permissions) use ($security): bool {
-                return $security->hasAnyPermission($object, $permissions);
+            static function ($arguments, $object, $permissions): bool {
+                return $arguments['globalVariable']->get('security')->hasAnyPermission($object, $permissions);
             }
         );
     }
--- tests/ExpressionLanguage/ExpressionFunction/Security/HasAnyPermissionTest.php
+++ tests/ExpressionLanguage/ExpressionFunction/Security/HasAnyPermissionTest.php
@@ -4,45 +4,53 @@ declare(strict_types=1);
 
 namespace Overblog\GraphQLBundle\Tests\ExpressionLanguage\ExpressionFunction\Security;
 
+use Overblog\GraphQLBundle\Definition\GlobalVariables;
 use Overblog\GraphQLBundle\ExpressionLanguage\ExpressionFunction\Security\HasAnyPermission;
 use Overblog\GraphQLBundle\Tests\ExpressionLanguage\TestCase;
 
 class HasAnyPermissionTest extends TestCase
 {
-    private $expectedObject;
     private $testedExpression = 'hasAnyPermission(object,["OWNER", "WRITER"])';
 
     protected function getFunctions()
     {
-        $this->expectedObject = new \stdClass();
-
-        $security = $this->getSecurityIsGrantedWithExpectation(
-            [
-                $this->matchesRegularExpression('/^(OWNER|WRITER)$/'),
-                $this->identicalTo($this->expectedObject),
-            ],
-            $this->any()
-        );
-
-        return [new HasAnyPermission($security)];
+        return [new HasAnyPermission()];
     }
 
     public function testEvaluator(): void
     {
-        $hasPermission = $this->expressionLanguage->evaluate($this->testedExpression, ['object' => $this->expectedObject]);
+        $expectedObject = new \stdClass();
+        $security       = $this->getSecurityIsGrantedWithExpectation(
+            [
+                $this->matchesRegularExpression('/^(OWNER|WRITER)$/'),
+                $this->identicalTo($expectedObject),
+            ],
+            $this->any()
+        );
+        $globalVariable = new GlobalVariables(['security' => $security]);
+
+        $hasPermission = $this->expressionLanguage->evaluate(
+            $this->testedExpression,
+            [
+                'globalVariable' => $globalVariable,
+                'object'         => $expectedObject,
+            ]
+        );
         $this->assertTrue($hasPermission);
     }
 
     public function testHasAnyPermission(): void
     {
+        $expectedObject = new \stdClass();
+
         $this->assertExpressionCompile(
             $this->testedExpression,
             [
                 $this->matchesRegularExpression('/^(OWNER|WRITER)$/'),
-                $this->identicalTo($this->expectedObject),
+                $this->identicalTo($expectedObject),
             ],
             [
-                'object' => $this->expectedObject,
+                'object' => $expectedObject,
             ]
         );
 
@@ -50,10 +58,10 @@ class HasAnyPermissionTest extends TestCase
             $this->testedExpression,
             [
                 $this->matchesRegularExpression('/^(OWNER|WRITER)$/'),
-                $this->identicalTo($this->expectedObject),
+                $this->identicalTo($expectedObject),
             ],
             [
-                'object' => $this->expectedObject,
+                'object' => $expectedObject,
             ],
             $this->exactly(2),
             false,
