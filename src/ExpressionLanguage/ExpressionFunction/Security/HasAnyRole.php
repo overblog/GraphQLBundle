41eae1d3c48a687a97be5342b5e09950bab76214
Stefan Gehrig <gehrig@teqneers.de>
2020-07-08 10:19:21 +0200

    removes security dependency from HasAnyRole function constructor

--- src/ExpressionLanguage/ExpressionFunction/Security/HasAnyRole.php
+++ src/ExpressionLanguage/ExpressionFunction/Security/HasAnyRole.php
@@ -5,19 +5,18 @@ declare(strict_types=1);
 namespace Overblog\GraphQLBundle\ExpressionLanguage\ExpressionFunction\Security;
 
 use Overblog\GraphQLBundle\ExpressionLanguage\ExpressionFunction;
-use Overblog\GraphQLBundle\Security\Security;
 
 final class HasAnyRole extends ExpressionFunction
 {
-    public function __construct(Security $security)
+    public function __construct()
     {
         parent::__construct(
             'hasAnyRole',
             static function ($roles): string {
                 return \sprintf('$globalVariable->get(\'security\')->hasAnyRole(%s)', $roles);
             },
-            static function ($_, $roles) use ($security): bool {
-                return $security->hasAnyRole($roles);
+            static function ($arguments, $roles): bool {
+                return $arguments['globalVariable']->get('security')->hasAnyRole($roles);
             }
         );
     }
