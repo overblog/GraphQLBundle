fc9517ae98f424d5da2413db29f261f247141d4e
Stefan Gehrig <gehrig@teqneers.de>
2020-07-08 10:24:28 +0200

    removes security dependency from IsAnonymous function constructor

--- tests/ExpressionLanguage/ExpressionFunction/Security/IsAnonymousTest.php
+++ tests/ExpressionLanguage/ExpressionFunction/Security/IsAnonymousTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Overblog\GraphQLBundle\Tests\ExpressionLanguage\ExpressionFunction\Security;
 
+use Overblog\GraphQLBundle\Definition\GlobalVariables;
 use Overblog\GraphQLBundle\ExpressionLanguage\ExpressionFunction\Security\IsAnonymous;
 use Overblog\GraphQLBundle\Tests\ExpressionLanguage\TestCase;
 
@@ -11,17 +12,18 @@ class IsAnonymousTest extends TestCase
 {
     protected function getFunctions()
     {
-        $Security = $this->getSecurityIsGrantedWithExpectation(
-            'IS_AUTHENTICATED_ANONYMOUSLY',
-            $this->any()
-        );
-
-        return [new IsAnonymous($Security)];
+        return [new IsAnonymous()];
     }
 
     public function testEvaluator(): void
     {
-        $isAnonymous = $this->expressionLanguage->evaluate('isAnonymous()');
+        $security       = $this->getSecurityIsGrantedWithExpectation(
+            'IS_AUTHENTICATED_ANONYMOUSLY',
+            $this->any()
+        );
+        $globalVariable = new GlobalVariables(['security' => $security]);
+
+        $isAnonymous = $this->expressionLanguage->evaluate('isAnonymous()', ['globalVariable' => $globalVariable]);
         $this->assertTrue($isAnonymous);
     }
 
