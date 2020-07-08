666113a2d00a8dacc5870d9c8cd9a44818e56dbb
Stefan Gehrig <gehrig@teqneers.de>
2020-07-08 10:26:50 +0200

    removes security dependency from IsFullyAuthenticated function constructor

--- src/ExpressionLanguage/ExpressionFunction/Security/IsFullyAuthenticated.php
+++ src/ExpressionLanguage/ExpressionFunction/Security/IsFullyAuthenticated.php
@@ -5,19 +5,18 @@ declare(strict_types=1);
 namespace Overblog\GraphQLBundle\ExpressionLanguage\ExpressionFunction\Security;
 
 use Overblog\GraphQLBundle\ExpressionLanguage\ExpressionFunction;
-use Overblog\GraphQLBundle\Security\Security;
 
 final class IsFullyAuthenticated extends ExpressionFunction
 {
-    public function __construct(Security $security)
+    public function __construct()
     {
         parent::__construct(
             'isFullyAuthenticated',
             static function (): string {
                 return '$globalVariable->get(\'security\')->isFullyAuthenticated()';
             },
-            static function () use ($security): bool {
-                return $security->isFullyAuthenticated();
+            static function ($arguments): bool {
+                return $arguments['globalVariable']->get('security')->isFullyAuthenticated();
             }
         );
     }
