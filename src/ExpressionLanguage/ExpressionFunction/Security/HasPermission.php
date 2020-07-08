c70499d32010977fcd46579dd2f707e96252c37d
Stefan Gehrig <gehrig@teqneers.de>
2020-07-08 10:21:40 +0200

    removes security dependency from HasPermission function constructor

--- src/ExpressionLanguage/ExpressionFunction/Security/HasPermission.php
+++ src/ExpressionLanguage/ExpressionFunction/Security/HasPermission.php
@@ -5,19 +5,18 @@ declare(strict_types=1);
 namespace Overblog\GraphQLBundle\ExpressionLanguage\ExpressionFunction\Security;
 
 use Overblog\GraphQLBundle\ExpressionLanguage\ExpressionFunction;
-use Overblog\GraphQLBundle\Security\Security;
 
 final class HasPermission extends ExpressionFunction
 {
-    public function __construct(Security $security)
+    public function __construct()
     {
         parent::__construct(
             'hasPermission',
             static function ($object, $permission): string {
                 return \sprintf('$globalVariable->get(\'security\')->hasPermission(%s, %s)', $object, $permission);
             },
-            static function ($_, $object, $permission) use ($security): bool {
-                return $security->hasPermission($object, $permission);
+            static function ($arguments, $object, $permission): bool {
+                return $arguments['globalVariable']->get('security')->hasPermission($object, $permission);
             }
         );
     }
--- tests/ExpressionLanguage/ExpressionFunction/Security/HasPermissionTest.php
+++ tests/ExpressionLanguage/ExpressionFunction/Security/HasPermissionTest.php
@@ -4,45 +4,52 @@ declare(strict_types=1);
 
 namespace Overblog\GraphQLBundle\Tests\ExpressionLanguage\ExpressionFunction\Security;
 
+use Overblog\GraphQLBundle\Definition\GlobalVariables;
 use Overblog\GraphQLBundle\ExpressionLanguage\ExpressionFunction\Security\HasPermission;
 use Overblog\GraphQLBundle\Tests\ExpressionLanguage\TestCase;
 
 class HasPermissionTest extends TestCase
 {
-    private $expectedObject;
     private $testedExpression = 'hasPermission(object,"OWNER")';
 
     protected function getFunctions()
     {
-        $this->expectedObject = new \stdClass();
-
-        $Security = $this->getSecurityIsGrantedWithExpectation(
-            [
-                'OWNER',
-                $this->identicalTo($this->expectedObject),
-            ],
-            $this->any()
-        );
-
-        return [new HasPermission($Security)];
+        return [new HasPermission()];
     }
 
     public function testEvaluator(): void
     {
-        $hasPermission = $this->expressionLanguage->evaluate($this->testedExpression, ['object' => $this->expectedObject]);
+        $expectedObject = new \stdClass();
+        $security       = $this->getSecurityIsGrantedWithExpectation(
+            [
+                'OWNER',
+                $this->identicalTo($expectedObject),
+            ],
+            $this->any()
+        );
+        $globalVariable = new GlobalVariables(['security' => $security]);
+
+        $hasPermission = $this->expressionLanguage->evaluate(
+            $this->testedExpression,
+            [
+                'globalVariable' => $globalVariable,
+                'object'         => $expectedObject,
+            ]
+        );
         $this->assertTrue($hasPermission);
     }
 
     public function testHasPermission(): void
     {
+        $expectedObject = new \stdClass();
         $this->assertExpressionCompile(
             $this->testedExpression,
             [
                 'OWNER',
-                $this->identicalTo($this->expectedObject),
+                $this->identicalTo($expectedObject),
             ],
             [
-                'object' => $this->expectedObject,
+                'object' => $expectedObject,
             ]
         );
     }
