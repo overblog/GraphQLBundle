28194dd658d6bb99ba21027b8914541705bae443
Stefan Gehrig <gehrig@teqneers.de>
2020-07-08 10:29:09 +0200

    removes security dependency from IsGranted function constructor

--- tests/ExpressionLanguage/ExpressionFunction/Security/IsGrantedTest.php
+++ tests/ExpressionLanguage/ExpressionFunction/Security/IsGrantedTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Overblog\GraphQLBundle\Tests\ExpressionLanguage\ExpressionFunction\Security;
 
+use Overblog\GraphQLBundle\Definition\GlobalVariables;
 use Overblog\GraphQLBundle\ExpressionLanguage\ExpressionFunction\Security\IsGranted;
 use Overblog\GraphQLBundle\Tests\ExpressionLanguage\TestCase;
 
@@ -11,17 +12,20 @@ class IsGrantedTest extends TestCase
 {
     protected function getFunctions()
     {
-        $security = $this->getSecurityIsGrantedWithExpectation(
-            $this->matchesRegularExpression('/^ROLE_(USER|ADMIN)$/'),
-            $this->any()
-        );
-
-        return [new IsGranted($security)];
+        return [new IsGranted()];
     }
 
     public function testEvaluator(): void
     {
-        $this->assertTrue($this->expressionLanguage->evaluate('isGranted("ROLE_USER")'));
+        $security       = $this->getSecurityIsGrantedWithExpectation(
+            $this->matchesRegularExpression('/^ROLE_(USER|ADMIN)$/'),
+            $this->any()
+        );
+        $globalVariable = new GlobalVariables(['security' => $security]);
+
+        $this->assertTrue(
+            $this->expressionLanguage->evaluate('isGranted("ROLE_USER")', ['globalVariable' => $globalVariable])
+        );
     }
 
     public function testIsGranted(): void
