96208d05d27d4f3de1f24220c53e0a10d8675e26
Damien Belingheri <belingheridamien@gmail.com>
2021-06-22 16:41:15 +0200

    Update AnnotationParser.php
    Update AnnotationParser.php
    
    Update AnnotationParser.php

--- src/Config/Parser/AnnotationParser.php
+++ src/Config/Parser/AnnotationParser.php
@@ -6,18 +6,22 @@ namespace Overblog\GraphQLBundle\Config\Parser;
 
 use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\Common\Annotations\PsrCachedReader;
+use Doctrine\Common\Annotations\Reader;
 use Overblog\GraphQLBundle\Config\Parser\MetadataParser\MetadataParser;
 use ReflectionClass;
 use ReflectionMethod;
 use ReflectionProperty;
 use Reflector;
 use RuntimeException;
+use Symfony\Component\Cache\Adapter\ApcuAdapter;
+use Symfony\Component\Cache\Adapter\PhpFilesAdapter;
 
 class AnnotationParser extends MetadataParser
 {
     public const METADATA_FORMAT = '@%s';
 
-    protected static ?AnnotationReader $annotationReader = null;
+    protected static Reader $annotationReader;
 
     protected static function getMetadatas(Reflector $reflector): array
     {
@@ -32,18 +36,28 @@ class AnnotationParser extends MetadataParser
         return [];
     }
 
-    protected static function getAnnotationReader(): AnnotationReader
+    protected static function getAnnotationReader(): PsrCachedReader
     {
-        if (null === self::$annotationReader) {
-            if (!class_exists(AnnotationReader::class) ||
-                !class_exists(AnnotationRegistry::class)) {
+        if (!isset(self::$annotationReader)) {
+            if (!class_exists(AnnotationReader::class) || !class_exists(AnnotationRegistry::class)) {
                 // @codeCoverageIgnoreStart
-                throw new RuntimeException('In order to use graphql annotation, you need to require doctrine annotations');
+                throw new RuntimeException("In order to use annotations, you need to install 'doctrine/annotations' first. See: 'https://www.doctrine-project.org/projects/annotations.html'");
                 // @codeCoverageIgnoreEnd
             }
 
-            AnnotationRegistry::registerLoader('class_exists');
-            self::$annotationReader = new AnnotationReader();
+            if (class_exists(AnnotationRegistry::class)) {
+                AnnotationRegistry::registerLoader('class_exists');
+            }
+            $cacheKey = md5(__DIR__);
+            // @codeCoverageIgnoreStart
+            if (extension_loaded('apcu') && apcu_enabled()) {
+                $annotationCache = new ApcuAdapter($cacheKey);
+            } else {
+                $annotationCache = new PhpFilesAdapter($cacheKey);
+            }
+            // @codeCoverageIgnoreEnd
+
+            self::$annotationReader = new PsrCachedReader(new AnnotationReader(), $annotationCache, true);
         }
 
         return self::$annotationReader;
