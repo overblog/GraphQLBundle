fd758c4b8b385d3fa7e365ff44dc86d9869daf5a
Ruud Kamphuis <ruudk@users.noreply.github.com>
2021-12-06 20:35:12 +0100

    Fix PHP 8.1 deprecation
    In PHP 8.1 optional parameters cannot be defined before required parameters.
    
    Because `string name = null` is optional, we also have to make `string $resolveType`optional. This way it works on PHP 8.1 without breaking BC.
    
    In 1.0, we should change the order.

--- src/Annotation/TypeInterface.php
+++ src/Annotation/TypeInterface.php
@@ -31,8 +31,12 @@ final class TypeInterface extends Annotation
      * @param string|null $name        The GraphQL name of the interface
      * @param string      $resolveType The express resolve type
      */
-    public function __construct(string $name = null, string $resolveType)
+    public function __construct(string $name = null, string $resolveType = null)
     {
+        // TODO: 1.0: Remove optionality for both parameters.
+        // Previously, only the name was optional, but resolveType was always required
+        // But in PHP 8.1 you cannot define an optional parameter before a required one.
+        // To not break BC we now also make the resolveType optional.
         $this->name = $name;
         $this->resolveType = $resolveType;
     }
