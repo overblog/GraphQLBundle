7781954b939c03d605231cd7a1560a3317d4f143
Jeremiah VALERIE <jeremiah@overblog.com>
2021-12-08 21:39:46 +0100

    Upgrade php-cs-fixer to 3.3.2

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -6,23 +6,34 @@ $finder = PhpCsFixer\Finder::create()
     ->in([__DIR__.'/src', __DIR__.'/tests'])
 ;
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules(
         [
             '@Symfony' => true,
-            '@PHP74Migration' => true,
-            '@PHP74Migration:risky' => true,
+            '@PHP80Migration' => true,
+            '@PHP80Migration:risky' => true,
             'ordered_imports' => ['imports_order' => ['class', 'function', 'const']],
-            'general_phpdoc_annotation_remove' => ['author', 'category', 'copyright', 'created', 'license', 'package', 'since', 'subpackage', 'version'],
+            'general_phpdoc_annotation_remove' => [
+                'annotations' => [
+                    'author',
+                    'category',
+                    'copyright',
+                    'created',
+                    'license',
+                    'package',
+                    'since',
+                    'subpackage',
+                    'version',
+                ],
+            ],
             'fully_qualified_strict_types' => true,
             'single_line_throw' => false,
             'phpdoc_to_comment' => false,
             'no_superfluous_phpdoc_tags' => ['allow_mixed' => true],
             'global_namespace_import' => ['import_functions' => true, 'import_classes' => true, 'import_constants' => true],
             'phpdoc_summary' => false,
-            'hash_to_slash_comment' => false,
             'single_line_comment_style' => false,
-            'phpdoc_no_alias_tag' => ['type' => 'var'],
+            'phpdoc_no_alias_tag' => ['replacements' => ['type' => 'var']],
             'no_mixed_echo_print' => ['use' => 'echo'],
         ]
     )
