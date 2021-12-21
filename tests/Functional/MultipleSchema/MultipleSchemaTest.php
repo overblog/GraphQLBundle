18a0fa0db5840e773277f6cfd76c8d3ba4caf525
Jeremiah VALERIE <jeremiah@overblog.com>
2021-12-21 11:29:32 +0100

    Remove useless class import

--- tests/Functional/MultipleSchema/MultipleSchemaTest.php
+++ tests/Functional/MultipleSchema/MultipleSchemaTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Overblog\GraphQLBundle\Tests\Functional\MultipleSchema;
 
-use GraphQL\Error\InvariantViolation;
 use Overblog\GraphQLBundle\Tests\Functional\TestCase;
 
 final class MultipleSchemaTest extends TestCase
