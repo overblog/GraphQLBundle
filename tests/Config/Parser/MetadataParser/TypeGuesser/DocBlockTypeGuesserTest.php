600a1db5cccfd1d41f35f47fe017ecbb5d61eb88
Jeremiah VALERIE <jeremiah@overblog.com>
2022-01-05 17:29:46 +0100

    Fix failling and refactor DocBlockTypeGuesserTest

--- tests/Config/Parser/MetadataParser/TypeGuesser/DocBlockTypeGuesserTest.php
+++ tests/Config/Parser/MetadataParser/TypeGuesser/DocBlockTypeGuesserTest.php
@@ -20,40 +20,75 @@ final class DocBlockTypeGuesserTest extends TestCase
         ReflectionMethod::class => 'return',
     ];
 
-    public function testGuess(): void
+    /**
+     * @dataProvider guessSuccessDataProvider
+     */
+    public function testGuessSuccess(string $docType, string $gqlType, ?ClassesTypesMap $map, ?string $reflectorClass): void
+    {
+        $docBlockGuesser = new DocBlockTypeGuesser($map ?: new ClassesTypesMap());
+        $this->assertEquals(
+            $gqlType,
+            $docBlockGuesser->guessType(
+                new ReflectionClass(__CLASS__),
+                $this->getMockedReflector($docType, $reflectorClass ?? ReflectionProperty::class)
+            )
+        );
+    }
+
+    public function guessSuccessDataProvider(): iterable
     {
         foreach ($this->reflectors as $reflectorClass => $tag) {
-            $this->doTest('string', 'String!', null, $reflectorClass);
-            $this->doTest('?string', 'String', null, $reflectorClass);
-            $this->doTest('string|null', 'String', null, $reflectorClass);
-            $this->doTest('string[]', '[String!]!', null, $reflectorClass);
-            $this->doTest('array<string>', '[String!]!', null, $reflectorClass);
-            $this->doTest('array<string>|null', '[String!]', null, $reflectorClass);
-            $this->doTest('array<string|null>|null', '[String]', null, $reflectorClass);
-            $this->doTest('int', 'Int!', null, $reflectorClass);
-            $this->doTest('integer', 'Int!', null, $reflectorClass);
-            $this->doTest('boolean', 'Boolean!', null, $reflectorClass);
-            $this->doTest('bool', 'Boolean!', null, $reflectorClass);
-            $this->doTest('float', 'Float!', null, $reflectorClass);
-            $this->doTest('double', 'Float!', null, $reflectorClass);
-            $this->doTest('iterable<string>', '[String!]!', null, $reflectorClass);
+            yield ['string', 'String!', null, $reflectorClass];
+            yield ['?string', 'String', null, $reflectorClass];
+            yield ['string|null', 'String', null, $reflectorClass];
+            yield ['string[]', '[String!]!', null, $reflectorClass];
+            yield ['array<string>', '[String!]!', null, $reflectorClass];
+            yield ['array<string>|null', '[String!]', null, $reflectorClass];
+            yield ['array<string|null>|null', '[String]', null, $reflectorClass];
+            yield ['int', 'Int!', null, $reflectorClass];
+            yield ['integer', 'Int!', null, $reflectorClass];
+            yield ['boolean', 'Boolean!', null, $reflectorClass];
+            yield ['bool', 'Boolean!', null, $reflectorClass];
+            yield ['float', 'Float!', null, $reflectorClass];
+            yield ['double', 'Float!', null, $reflectorClass];
+            yield ['iterable<string>', '[String!]!', null, $reflectorClass];
+        }
 
-            $this->doTestError('int|float', $reflectorClass, 'Tag @'.$tag.' found, but composite types are only allowed with null');
-            $this->doTestError('array<int|float>', $reflectorClass, 'Tag @'.$tag.' found, but composite types in array or iterable are only allowed with null');
-            $this->doTestError('UnknownClass', $reflectorClass, 'Tag @'.$tag.' found, but target object "Overblog\GraphQLBundle\Tests\Config\Parser\UnknownClass" is not a GraphQL Type class');
-            $this->doTestError('object', $reflectorClass, 'Tag @'.$tag.' found, but type "object" is too generic');
-            $this->doTestError('mixed[]', $reflectorClass, 'Tag @'.$tag.' found, but the array values cannot be mixed type');
-            $this->doTestError('array<mixed>', $reflectorClass, 'Tag @'.$tag.' found, but the array values cannot be mixed type');
-            $this->doTestError('', $reflectorClass, 'No @'.$tag.' tag found in doc block or tag has no type');
-            $this->doTestError('[]', $reflectorClass, 'Doc Block parsing failed');
+        $map = new ClassesTypesMap();
+        $map->addClassType('GQLType1', 'Fake\Class1', 'object');
+        $map->addClassType('GQLType2', 'Fake\Class2', 'object');
+        $map->addClassType('Foo', ClassesTypesMap::class, 'object');
 
-            $map = new ClassesTypesMap();
-            $map->addClassType('GQLType1', 'Fake\Class1', 'object');
-            $map->addClassType('GQLType2', 'Fake\Class2', 'object');
-            $map->addClassType('Foo', ClassesTypesMap::class, 'object');
+        yield ['\Fake\Class1[]', '[GQLType1!]!', $map, null];
+        yield ['ClassesTypesMap|null', 'Foo', $map, null];
+    }
 
-            $this->doTest('\Fake\Class1[]', '[GQLType1!]!', $map);
-            $this->doTest('ClassesTypesMap|null', 'Foo', $map);
+    /**
+     * @dataProvider guessErrorDataProvider
+     */
+    public function testGuessError(string $docType, string $reflectorClass, string $match): void
+    {
+        $docBlockGuesser = new DocBlockTypeGuesser(new ClassesTypesMap());
+        try {
+            $docBlockGuesser->guessType(new ReflectionClass(__CLASS__), $this->getMockedReflector($docType, $reflectorClass));
+            $this->fail(sprintf('The @var "%s" should resolve to GraphQL type "%s"', $docType, $match));
+        } catch (Exception $e) {
+            $this->assertInstanceOf(TypeGuessingException::class, $e);
+            $this->assertStringContainsString($match, $e->getMessage());
+        }
+    }
+
+    public function guessErrorDataProvider(): iterable
+    {
+        foreach ($this->reflectors as $reflectorClass => $tag) {
+            yield ['int|float', $reflectorClass, 'Tag @'.$tag.' found, but composite types are only allowed with null'];
+            yield ['array<int|float>', $reflectorClass, 'Tag @'.$tag.' found, but composite types in array or iterable are only allowed with null'];
+            yield ['UnknownClass', $reflectorClass, 'Tag @'.$tag.' found, but target object "Overblog\GraphQLBundle\Tests\Config\Parser\UnknownClass" is not a GraphQL Type class'];
+            yield ['object', $reflectorClass, 'Tag @'.$tag.' found, but type "object" is too generic'];
+            yield ['mixed[]', $reflectorClass, 'Tag @'.$tag.' found, but the array values cannot be mixed type'];
+            yield ['array<mixed>', $reflectorClass, 'Tag @'.$tag.' found, but the array values cannot be mixed type'];
+            yield ['', $reflectorClass, 'No @'.$tag.' tag found in doc block or tag has no type'];
+            yield ['[]', $reflectorClass, 'Doc Block parsing failed'];
         }
     }
 
@@ -71,28 +106,10 @@ final class DocBlockTypeGuesserTest extends TestCase
         }
     }
 
-    protected function doTest(string $docType, string $gqlType, ClassesTypesMap $map = null, string $reflectorClass = ReflectionProperty::class): void
-    {
-        $docBlockGuesser = new DocBlockTypeGuesser($map ?: new ClassesTypesMap());
-        $this->assertEquals($gqlType, $docBlockGuesser->guessType(new ReflectionClass(__CLASS__), $this->getMockedReflector($docType, $reflectorClass)));
-    }
-
-    protected function doTestError(string $docType, string $reflectorClass, string $match): void
-    {
-        $docBlockGuesser = new DocBlockTypeGuesser(new ClassesTypesMap());
-        try {
-            $docBlockGuesser->guessType(new ReflectionClass(__CLASS__), $this->getMockedReflector($docType, $reflectorClass));
-            $this->fail(sprintf('The @var "%s" should resolve to GraphQL type "%s"', $docType, $match));
-        } catch (Exception $e) {
-            $this->assertInstanceOf(TypeGuessingException::class, $e);
-            $this->assertStringContainsString($match, $e->getMessage());
-        }
-    }
-
     /**
      * @return ReflectionProperty|ReflectionMethod
      */
-    protected function getMockedReflector(string $type, string $className = ReflectionProperty::class)
+    private function getMockedReflector(string $type, string $className = ReflectionProperty::class)
     {
         // @phpstan-ignore-next-line
         $mock = $this->createMock($className);
