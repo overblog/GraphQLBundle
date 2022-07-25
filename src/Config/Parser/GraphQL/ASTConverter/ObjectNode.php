c5eb2ca48c8a82fbfcdf8f2cac1e8b6d1c40bc2d
Anatoliy Melnikau <amelnikov@aristeksystems.com>
2022-07-25 19:01:44 +0300

    Make ObjectNode.php config parsing better overridable.

--- src/Config/Parser/GraphQL/ASTConverter/ObjectNode.php
+++ src/Config/Parser/GraphQL/ASTConverter/ObjectNode.php
@@ -5,12 +5,26 @@ declare(strict_types=1);
 namespace Overblog\GraphQLBundle\Config\Parser\GraphQL\ASTConverter;
 
 use GraphQL\Language\AST\Node;
+use GraphQL\Language\AST\ObjectTypeDefinitionNode;
 
 class ObjectNode implements NodeInterface
 {
     protected const TYPENAME = 'object';
 
     public static function toConfig(Node $node): array
+    {
+        return [
+            'type' => static::TYPENAME,
+            'config' => static::parseConfig($node),
+        ];
+    }
+
+    /**
+     * @param ObjectTypeDefinitionNode $node
+     *
+     * @return array<string,mixed>
+     */
+    protected static function parseConfig(Node $node): array
     {
         $config = DescriptionNode::toConfig($node) + [
             'fields' => FieldsNode::toConfig($node),
@@ -24,9 +38,6 @@ class ObjectNode implements NodeInterface
             $config['interfaces'] = $interfaces;
         }
 
-        return [
-            'type' => static::TYPENAME,
-            'config' => $config,
-        ];
+        return $config;
     }
 }
