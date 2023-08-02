e9616ab10845fd0659d329366dcfed3a2369085e
Gunnstein Lye <289744+glye@users.noreply.github.com>
2023-08-02 13:52:13 +0200

    Add missing use statement

--- src/Controller/GraphController.php
+++ src/Controller/GraphController.php
@@ -8,6 +8,7 @@ use Overblog\GraphQLBundle\Request as GraphQLRequest;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class GraphController
 {
